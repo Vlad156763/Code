29f2f4ed2aea2da74c867c60de81782033492050
Влад Гайлунь <vladgaulun@gmail.com>
2024-11-05 11:55:59 +0200

    Стабільна робота видалення, додавання, виведення ключів у В-деерева. Коректна обробка видалення динамічної пам'яті.

--- Code/lb_2/B_tree.h
+++ Code/lb_2/B_tree.h
@@ -1,28 +1,51 @@
 #ifndef B_tree
 #define B_tree
 #include <iostream>
+#include "../lb_1/Data_structures.h"
+using data_struct::dl_list;
 using std::ostream;
 
+void Fhome_Btree(dl_list<const char*>&, bool&);
 namespace data_struct_b_tree {
-	void test_b_tree();
+	
 
 	template<typename T> class b_tree {
 	private:
+		/*
+		порядок дерева t
+		кількість ключів у вузлі 2t - 1
+		кількість покажчиків на нащадків 2t
+		мінімальна кількість ключів у вузлі t - 1
+		*/
+		int t = 0;
 		int max_num_keys = 0;
+		int max_num_children = 0;
+		int min_num_keys = 0;
 		struct node{
 		public:
+			int t = 0; //порядок дерева (потрібно для видалення 
 			T* array_keys = nullptr; //масив ключів
 			node** children = nullptr; //масив покажчиків node*
 			bool leaf = false; //true якщо вузол листок, і  false якщо ні
-			int num_keys = 0;
-			node* father = nullptr;
-			node(const int &, const bool&, node*);	
+			int num_keys = 0; //кількість ключів означає 2*t це не порядок дерева
+			node* father = nullptr; //покажчик на батька
+			node(const int &, const bool&, node*);
+			~node();
 		}*root = nullptr;
-		typename b_tree<T>::node* split_node(node*);
-		bool key_is_in(const T&); //перевірка чи ключ є в дереві
+		node* split_node(node*); //розділення вузлів
+		node* key_is_in(const T&); //перевірка чи ключ є в дереві
 		void del_all_nodes(node*);//рекурсивне видалення всіх вузлів
+
+		//об'єднання вузлів
+		void merge(node*, const int&, const int&);
+		//позика у брата (перший параметр це покажчик на поточний вузол, другий параметр це індекс вузла відносно батька
+		void borrow(node*, const int&, const int&);
+		//видалення з листка
+		void deletedKeyFromLeaf(node*, const int&, const int&);
+		void deletedKeyFromNotLeaf(node*, const int&);
 	public:
 		void add_key(const T&);
+		void del_key(const T&);
 		void print(ostream&);
 		b_tree(const int&); //параметр const int& для передачі t
 		~b_tree(); 
--- Code/lb_2/main_lb_2.cpp
+++ Code/lb_2/main_lb_2.cpp
@@ -1,16 +1,67 @@
 #include "main_lb_2.h"
 #include "Exception_for_lb_2.h"
+#include "../lb_1/Data_structures.h"
 #include "B_tree.h"
 #include <iostream>
+#include <string>
+using data_struct::dl_list;
+using std::string;
 using std::cout;
 using std::endl;
-using data_struct_b_tree::test_b_tree;
-using my_exceptions_2::My_exception_codes_file;
+using std::cin;
+using std::numeric_limits;
+using std::streamsize;
+/*
+b-дерево:
+Вивід 
+додавання ключа
+видалення ключа
+пошук
 
+*/
 namespace task_2 {
 	void main_test_all_files() {
-		My_exception_codes_file();
-		test_b_tree();
+		my_exceptions_2::My_exception_codes_file();
+		bool exit_loop_home = false;
+		dl_list<const char*> way;
+		way.push_back("/home/");
+		do {
+			cout << "\033[38;2;100;100;255m";
+			for (int i = 0; i < way.get_size(); i++) cout << way[i];
+			cout << "\033[0m" << endl
+				<< "Завершити програму \t0 Enter" << endl
+				<< "B-дерево           \t1 Enter" << endl
+				<< "Геш Таблиця        \t2 Enter" << endl
+				<< "> ";
+
+			char b_tree_TestNew = cin.get();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n'); //ігнорую всі інші символи в буфері до наступного рядка
+			switch (b_tree_TestNew) {
+			case '0': {
+				exit_loop_home = true;
+				break;
+			}
+			case'1': {
+				Fhome_Btree(way, exit_loop_home);
+				break;
+			}
+			case'2': {
+				//Fhome_HachTable(way, exit_loop_home);
+				//Fhome_HachTable(way, exit_loop_home);
+				//Fhome_HachTable(way, exit_loop_home);
+				cout << "\033[38;2;255;0;0mНе зроблено! Вибачте :(\033[0m" << endl;
+#define NOT_IMPLEMENTED_1 0
+				break;
+			}
+			default:
+				cout << "\033[38;2;255;0;0m" << "Помилка! Обрано не вірний пункт" << "\033[0m" << endl;
+				continue;
+			}
+		} while (!exit_loop_home);
+		
+
+
+
 		cout
 			<< endl
 			<< "\033[38;2;0;255;0m"
@@ -18,5 +69,7 @@ namespace task_2 {
 			<< "| Програма завершила роботу! |" << endl
 			<< "+----------------------------+"
 			<< "\033[0m";
+		
 	}
+	
 }
